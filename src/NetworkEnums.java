ffbdbb5d545addec94a201adb821c9a2ceb28fdf
Abolfazl <moslemiabolfazl14@gmail.com>
2025-04-23 08:46:34 +0330

    first=Initialize base game structure

---
+++ src/NetworkEnums.java
@@ -0,0 +1,19 @@
+public class NetworkEnums {
+    private NetworkEnums() {}
+
+    public enum PortType {
+        INPUT,
+        OUTPUT
+    }
+
+    public enum PortShape {
+        SQUARE,
+        TRIANGLE,
+        ANY
+    }
+
+    public enum PacketShape {
+        SQUARE,
+        TRIANGLE
+    }
+}
---
+++ src/NetworkGame.java
@@ -0,0 +1,19 @@
+import javax.swing.*;
+
+public class NetworkGame extends JFrame {
+    public static final int WINDOW_WIDTH = 1200;
+    public static final int WINDOW_HEIGHT = 800;
+
+    public NetworkGame() {
+        setTitle("Network Operator Simulator");
+        setSize(WINDOW_WIDTH, WINDOW_HEIGHT);
+        setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        setResizable(false);
+        setLocationRelativeTo(null);
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(NetworkGame::new);
+    }
+}
